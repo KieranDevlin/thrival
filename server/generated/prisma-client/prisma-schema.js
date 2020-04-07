54a175575d43c8bc29719b3d5ad5c3323ba7ff84
Ivan <ivandaixivwork@gmail.com>
2020-04-07 16:24:09 -0700

    Adds relation

--- server/generated/prisma-client/prisma-schema.js
+++ server/generated/prisma-client/prisma-schema.js
@@ -28,6 +28,7 @@ type Applicant {
   email: String!
   linkedin: String!
   github: String!
+  appliedJobs(where: JobPostWhereInput, orderBy: JobPostOrderByInput, skip: Int, after: String, before: String, first: Int, last: Int): [JobPost!]
 }
 
 type ApplicantConnection {
@@ -37,6 +38,24 @@ type ApplicantConnection {
 }
 
 input ApplicantCreateInput {
+  id: ID
+  email: String!
+  linkedin: String!
+  github: String!
+  appliedJobs: JobPostCreateManyWithoutViewedInput
+}
+
+input ApplicantCreateManyWithoutAppliedJobsInput {
+  create: [ApplicantCreateWithoutAppliedJobsInput!]
+  connect: [ApplicantWhereUniqueInput!]
+}
+
+input ApplicantCreateOneInput {
+  create: ApplicantCreateInput
+  connect: ApplicantWhereUniqueInput
+}
+
+input ApplicantCreateWithoutAppliedJobsInput {
   id: ID
   email: String!
   linkedin: String!
@@ -66,6 +85,68 @@ type ApplicantPreviousValues {
   github: String!
 }
 
+input ApplicantScalarWhereInput {
+  id: ID
+  id_not: ID
+  id_in: [ID!]
+  id_not_in: [ID!]
+  id_lt: ID
+  id_lte: ID
+  id_gt: ID
+  id_gte: ID
+  id_contains: ID
+  id_not_contains: ID
+  id_starts_with: ID
+  id_not_starts_with: ID
+  id_ends_with: ID
+  id_not_ends_with: ID
+  email: String
+  email_not: String
+  email_in: [String!]
+  email_not_in: [String!]
+  email_lt: String
+  email_lte: String
+  email_gt: String
+  email_gte: String
+  email_contains: String
+  email_not_contains: String
+  email_starts_with: String
+  email_not_starts_with: String
+  email_ends_with: String
+  email_not_ends_with: String
+  linkedin: String
+  linkedin_not: String
+  linkedin_in: [String!]
+  linkedin_not_in: [String!]
+  linkedin_lt: String
+  linkedin_lte: String
+  linkedin_gt: String
+  linkedin_gte: String
+  linkedin_contains: String
+  linkedin_not_contains: String
+  linkedin_starts_with: String
+  linkedin_not_starts_with: String
+  linkedin_ends_with: String
+  linkedin_not_ends_with: String
+  github: String
+  github_not: String
+  github_in: [String!]
+  github_not_in: [String!]
+  github_lt: String
+  github_lte: String
+  github_gt: String
+  github_gte: String
+  github_contains: String
+  github_not_contains: String
+  github_starts_with: String
+  github_not_starts_with: String
+  github_ends_with: String
+  github_not_ends_with: String
+  AND: [ApplicantScalarWhereInput!]
+  OR: [ApplicantScalarWhereInput!]
+  NOT: [ApplicantScalarWhereInput!]
+}
+
 type ApplicantSubscriptionPayload {
   mutation: MutationType!
   node: Applicant
@@ -84,7 +165,21 @@ input ApplicantSubscriptionWhereInput {
   NOT: [ApplicantSubscriptionWhereInput!]
 }
 
+input ApplicantUpdateDataInput {
+  email: String
+  linkedin: String
+  github: String
+  appliedJobs: JobPostUpdateManyWithoutViewedInput
+}
+
 input ApplicantUpdateInput {
+  email: String
+  linkedin: String
+  github: String
+  appliedJobs: JobPostUpdateManyWithoutViewedInput
+}
+
+input ApplicantUpdateManyDataInput {
   email: String
   linkedin: String
   github: String
@@ -94,6 +189,54 @@ input ApplicantUpdateManyMutationInput {
   email: String
   linkedin: String
   github: String
+}
+
+input ApplicantUpdateManyWithoutAppliedJobsInput {
+  create: [ApplicantCreateWithoutAppliedJobsInput!]
+  delete: [ApplicantWhereUniqueInput!]
+  connect: [ApplicantWhereUniqueInput!]
+  set: [ApplicantWhereUniqueInput!]
+  disconnect: [ApplicantWhereUniqueInput!]
+  update: [ApplicantUpdateWithWhereUniqueWithoutAppliedJobsInput!]
+  upsert: [ApplicantUpsertWithWhereUniqueWithoutAppliedJobsInput!]
+  deleteMany: [ApplicantScalarWhereInput!]
+  updateMany: [ApplicantUpdateManyWithWhereNestedInput!]
+}
+
+input ApplicantUpdateManyWithWhereNestedInput {
+  where: ApplicantScalarWhereInput!
+  data: ApplicantUpdateManyDataInput!
+}
+
+input ApplicantUpdateOneInput {
+  create: ApplicantCreateInput
+  update: ApplicantUpdateDataInput
+  upsert: ApplicantUpsertNestedInput
+  delete: Boolean
+  disconnect: Boolean
+  connect: ApplicantWhereUniqueInput
+}
+
+input ApplicantUpdateWithoutAppliedJobsDataInput {
+  email: String
+  linkedin: String
+  github: String
+}
+
+input ApplicantUpdateWithWhereUniqueWithoutAppliedJobsInput {
+  where: ApplicantWhereUniqueInput!
+  data: ApplicantUpdateWithoutAppliedJobsDataInput!
+}
+
+input ApplicantUpsertNestedInput {
+  update: ApplicantUpdateDataInput!
+  create: ApplicantCreateInput!
+}
+
+input ApplicantUpsertWithWhereUniqueWithoutAppliedJobsInput {
+  where: ApplicantWhereUniqueInput!
+  update: ApplicantUpdateWithoutAppliedJobsDataInput!
+  create: ApplicantCreateWithoutAppliedJobsInput!
 }
 
 input ApplicantWhereInput {
@@ -153,6 +296,9 @@ input ApplicantWhereInput {
   github_not_starts_with: String
   github_ends_with: String
   github_not_ends_with: String
+  appliedJobs_every: JobPostWhereInput
+  appliedJobs_some: JobPostWhereInput
+  appliedJobs_none: JobPostWhereInput
   AND: [ApplicantWhereInput!]
   OR: [ApplicantWhereInput!]
   NOT: [ApplicantWhereInput!]
@@ -183,6 +329,11 @@ input ContactCreateInput {
   id: ID
   email: String
   website: String
+}
+
+input ContactCreateOneInput {
+  create: ContactCreateInput
+  connect: ContactWhereUniqueInput
 }
 
 type ContactEdge {
@@ -223,6 +374,11 @@ input ContactSubscriptionWhereInput {
   NOT: [ContactSubscriptionWhereInput!]
 }
 
+input ContactUpdateDataInput {
+  email: String
+  website: String
+}
+
 input ContactUpdateInput {
   email: String
   website: String
@@ -231,6 +387,20 @@ input ContactUpdateInput {
 input ContactUpdateManyMutationInput {
   email: String
   website: String
+}
+
+input ContactUpdateOneInput {
+  create: ContactCreateInput
+  update: ContactUpdateDataInput
+  upsert: ContactUpsertNestedInput
+  delete: Boolean
+  disconnect: Boolean
+  connect: ContactWhereUniqueInput
+}
+
+input ContactUpsertNestedInput {
+  update: ContactUpdateDataInput!
+  create: ContactCreateInput!
 }
 
 input ContactWhereInput {
@@ -289,6 +459,8 @@ scalar DateTime
 
 type Employer {
   id: ID!
+  jobpostings(where: JobPostWhereInput, orderBy: JobPostOrderByInput, skip: Int, after: String, before: String, first: Int, last: Int): [JobPost!]
+  contact: Contact
 }
 
 type EmployerConnection {
@@ -299,6 +471,23 @@ type EmployerConnection {
 
 input EmployerCreateInput {
   id: ID
+  jobpostings: JobPostCreateManyWithoutEmployerInput
+  contact: ContactCreateOneInput
+}
+
+input EmployerCreateOneInput {
+  create: EmployerCreateInput
+  connect: EmployerWhereUniqueInput
+}
+
+input EmployerCreateOneWithoutJobpostingsInput {
+  create: EmployerCreateWithoutJobpostingsInput
+  connect: EmployerWhereUniqueInput
+}
+
+input EmployerCreateWithoutJobpostingsInput {
+  id: ID
+  contact: ContactCreateOneInput
 }
 
 type EmployerEdge {
@@ -333,6 +522,46 @@ input EmployerSubscriptionWhereInput {
   NOT: [EmployerSubscriptionWhereInput!]
 }
 
+input EmployerUpdateDataInput {
+  jobpostings: JobPostUpdateManyWithoutEmployerInput
+  contact: ContactUpdateOneInput
+}
+
+input EmployerUpdateInput {
+  jobpostings: JobPostUpdateManyWithoutEmployerInput
+  contact: ContactUpdateOneInput
+}
+
+input EmployerUpdateOneInput {
+  create: EmployerCreateInput
+  update: EmployerUpdateDataInput
+  upsert: EmployerUpsertNestedInput
+  delete: Boolean
+  disconnect: Boolean
+  connect: EmployerWhereUniqueInput
+}
+
+input EmployerUpdateOneRequiredWithoutJobpostingsInput {
+  create: EmployerCreateWithoutJobpostingsInput
+  update: EmployerUpdateWithoutJobpostingsDataInput
+  upsert: EmployerUpsertWithoutJobpostingsInput
+  connect: EmployerWhereUniqueInput
+}
+
+input EmployerUpdateWithoutJobpostingsDataInput {
+  contact: ContactUpdateOneInput
+}
+
+input EmployerUpsertNestedInput {
+  update: EmployerUpdateDataInput!
+  create: EmployerCreateInput!
+}
+
+input EmployerUpsertWithoutJobpostingsInput {
+  update: EmployerUpdateWithoutJobpostingsDataInput!
+  create: EmployerCreateWithoutJobpostingsInput!
+}
+
 input EmployerWhereInput {
   id: ID
   id_not: ID
@@ -348,6 +577,10 @@ input EmployerWhereInput {
   id_not_starts_with: ID
   id_ends_with: ID
   id_not_ends_with: ID
+  jobpostings_every: JobPostWhereInput
+  jobpostings_some: JobPostWhereInput
+  jobpostings_none: JobPostWhereInput
+  contact: ContactWhereInput
   AND: [EmployerWhereInput!]
   OR: [EmployerWhereInput!]
   NOT: [EmployerWhereInput!]
@@ -364,6 +597,8 @@ type JobPost {
   location: String!
   discipline: [String!]!
   totalRoles: Int!
+  employer: Employer!
+  viewed(where: ApplicantWhereInput, orderBy: ApplicantOrderByInput, skip: Int, after: String, before: String, first: Int, last: Int): [Applicant!]
 }
 
 type JobPostConnection {
@@ -382,6 +617,36 @@ input JobPostCreateInput {
   location: String!
   discipline: JobPostCreatedisciplineInput
   totalRoles: Int!
+  employer: EmployerCreateOneWithoutJobpostingsInput!
+  viewed: ApplicantCreateManyWithoutAppliedJobsInput
+}
+
+input JobPostCreateManyWithoutEmployerInput {
+  create: [JobPostCreateWithoutEmployerInput!]
+  connect: [JobPostWhereUniqueInput!]
+}
+
+input JobPostCreateManyWithoutViewedInput {
+  create: [JobPostCreateWithoutViewedInput!]
+  connect: [JobPostWhereUniqueInput!]
+}
+
+input JobPostCreateWithoutEmployerInput {
+  id: ID
+  industry: String!
+  location: String!
+  discipline: JobPostCreatedisciplineInput
+  totalRoles: Int!
+  viewed: ApplicantCreateManyWithoutAppliedJobsInput
+}
+
+input JobPostCreateWithoutViewedInput {
+  id: ID
+  industry: String!
+  location: String!
+  discipline: JobPostCreatedisciplineInput
+  totalRoles: Int!
+  employer: EmployerCreateOneWithoutJobpostingsInput!
 }
 
 type JobPostEdge {
@@ -411,6 +676,70 @@ type JobPostPreviousValues {
   totalRoles: Int!
 }
 
+input JobPostScalarWhereInput {
+  id: ID
+  id_not: ID
+  id_in: [ID!]
+  id_not_in: [ID!]
+  id_lt: ID
+  id_lte: ID
+  id_gt: ID
+  id_gte: ID
+  id_contains: ID
+  id_not_contains: ID
+  id_starts_with: ID
+  id_not_starts_with: ID
+  id_ends_with: ID
+  id_not_ends_with: ID
+  createdAt: DateTime
+  createdAt_not: DateTime
+  createdAt_in: [DateTime!]
+  createdAt_not_in: [DateTime!]
+  createdAt_lt: DateTime
+  createdAt_lte: DateTime
+  createdAt_gt: DateTime
+  createdAt_gte: DateTime
+  industry: String
+  industry_not: String
+  industry_in: [String!]
+  industry_not_in: [String!]
+  industry_lt: String
+  industry_lte: String
+  industry_gt: String
+  industry_gte: String
+  industry_contains: String
+  industry_not_contains: String
+  industry_starts_with: String
+  industry_not_starts_with: String
+  industry_ends_with: String
+  industry_not_ends_with: String
+  location: String
+  location_not: String
+  location_in: [String!]
+  location_not_in: [String!]
+  location_lt: String
+  location_lte: String
+  location_gt: String
+  location_gte: String
+  location_contains: String
+  location_not_contains: String
+  location_starts_with: String
+  location_not_starts_with: String
+  location_ends_with: String
+  location_not_ends_with: String
+  totalRoles: Int
+  totalRoles_not: Int
+  totalRoles_in: [Int!]
+  totalRoles_not_in: [Int!]
+  totalRoles_lt: Int
+  totalRoles_lte: Int
+  totalRoles_gt: Int
+  totalRoles_gte: Int
+  AND: [JobPostScalarWhereInput!]
+  OR: [JobPostScalarWhereInput!]
+  NOT: [JobPostScalarWhereInput!]
+}
+
 type JobPostSubscriptionPayload {
   mutation: MutationType!
   node: JobPost
@@ -438,6 +767,15 @@ input JobPostUpdateInput {
   location: String
   discipline: JobPostUpdatedisciplineInput
   totalRoles: Int
+  employer: EmployerUpdateOneRequiredWithoutJobpostingsInput
+  viewed: ApplicantUpdateManyWithoutAppliedJobsInput
+}
+
+input JobPostUpdateManyDataInput {
+  industry: String
+  location: String
+  discipline: JobPostUpdatedisciplineInput
+  totalRoles: Int
 }
 
 input JobPostUpdateManyMutationInput {
@@ -445,6 +783,73 @@ input JobPostUpdateManyMutationInput {
   location: String
   discipline: JobPostUpdatedisciplineInput
   totalRoles: Int
+}
+
+input JobPostUpdateManyWithoutEmployerInput {
+  create: [JobPostCreateWithoutEmployerInput!]
+  delete: [JobPostWhereUniqueInput!]
+  connect: [JobPostWhereUniqueInput!]
+  set: [JobPostWhereUniqueInput!]
+  disconnect: [JobPostWhereUniqueInput!]
+  update: [JobPostUpdateWithWhereUniqueWithoutEmployerInput!]
+  upsert: [JobPostUpsertWithWhereUniqueWithoutEmployerInput!]
+  deleteMany: [JobPostScalarWhereInput!]
+  updateMany: [JobPostUpdateManyWithWhereNestedInput!]
+}
+
+input JobPostUpdateManyWithoutViewedInput {
+  create: [JobPostCreateWithoutViewedInput!]
+  delete: [JobPostWhereUniqueInput!]
+  connect: [JobPostWhereUniqueInput!]
+  set: [JobPostWhereUniqueInput!]
+  disconnect: [JobPostWhereUniqueInput!]
+  update: [JobPostUpdateWithWhereUniqueWithoutViewedInput!]
+  upsert: [JobPostUpsertWithWhereUniqueWithoutViewedInput!]
+  deleteMany: [JobPostScalarWhereInput!]
+  updateMany: [JobPostUpdateManyWithWhereNestedInput!]
+}
+
+input JobPostUpdateManyWithWhereNestedInput {
+  where: JobPostScalarWhereInput!
+  data: JobPostUpdateManyDataInput!
+}
+
+input JobPostUpdateWithoutEmployerDataInput {
+  industry: String
+  location: String
+  discipline: JobPostUpdatedisciplineInput
+  totalRoles: Int
+  viewed: ApplicantUpdateManyWithoutAppliedJobsInput
+}
+
+input JobPostUpdateWithoutViewedDataInput {
+  industry: String
+  location: String
+  discipline: JobPostUpdatedisciplineInput
+  totalRoles: Int
+  employer: EmployerUpdateOneRequiredWithoutJobpostingsInput
+}
+
+input JobPostUpdateWithWhereUniqueWithoutEmployerInput {
+  where: JobPostWhereUniqueInput!
+  data: JobPostUpdateWithoutEmployerDataInput!
+}
+
+input JobPostUpdateWithWhereUniqueWithoutViewedInput {
+  where: JobPostWhereUniqueInput!
+  data: JobPostUpdateWithoutViewedDataInput!
+}
+
+input JobPostUpsertWithWhereUniqueWithoutEmployerInput {
+  where: JobPostWhereUniqueInput!
+  update: JobPostUpdateWithoutEmployerDataInput!
+  create: JobPostCreateWithoutEmployerInput!
+}
+
+input JobPostUpsertWithWhereUniqueWithoutViewedInput {
+  where: JobPostWhereUniqueInput!
+  update: JobPostUpdateWithoutViewedDataInput!
+  create: JobPostCreateWithoutViewedInput!
 }
 
 input JobPostWhereInput {
@@ -506,6 +911,10 @@ input JobPostWhereInput {
   totalRoles_lte: Int
   totalRoles_gt: Int
   totalRoles_gte: Int
+  employer: EmployerWhereInput
+  viewed_every: ApplicantWhereInput
+  viewed_some: ApplicantWhereInput
+  viewed_none: ApplicantWhereInput
   AND: [JobPostWhereInput!]
   OR: [JobPostWhereInput!]
   NOT: [JobPostWhereInput!]
@@ -531,6 +940,8 @@ type Mutation {
   deleteContact(where: ContactWhereUniqueInput!): Contact
   deleteManyContacts(where: ContactWhereInput): BatchPayload!
   createEmployer(data: EmployerCreateInput!): Employer!
+  updateEmployer(data: EmployerUpdateInput!, where: EmployerWhereUniqueInput!): Employer
+  upsertEmployer(where: EmployerWhereUniqueInput!, create: EmployerCreateInput!, update: EmployerUpdateInput!): Employer!
   deleteEmployer(where: EmployerWhereUniqueInput!): Employer
   deleteManyEmployers(where: EmployerWhereInput): BatchPayload!
   createJobPost(data: JobPostCreateInput!): JobPost!
@@ -595,6 +1006,8 @@ type User {
   id: ID!
   name: String!
   password: String!
+  employerProfile: Employer
+  applicantProfile: Applicant
 }
 
 type UserConnection {
@@ -607,6 +1020,8 @@ input UserCreateInput {
   id: ID
   name: String!
   password: String!
+  employerProfile: EmployerCreateOneInput
+  applicantProfile: ApplicantCreateOneInput
 }
 
 type UserEdge {
@@ -650,6 +1065,8 @@ input UserSubscriptionWhereInput {
 input UserUpdateInput {
   name: String
   password: String
+  employerProfile: EmployerUpdateOneInput
+  applicantProfile: ApplicantUpdateOneInput
 }
 
 input UserUpdateManyMutationInput {
@@ -700,6 +1117,8 @@ input UserWhereInput {
   password_not_starts_with: String
   password_ends_with: String
   password_not_ends_with: String
+  employerProfile: EmployerWhereInput
+  applicantProfile: ApplicantWhereInput
   AND: [UserWhereInput!]
   OR: [UserWhereInput!]
   NOT: [UserWhereInput!]
