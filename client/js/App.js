fe0f15b1f94320c69e69904dcd43f5b36f22ab5e
Ken Huang <khuang.dev@gmail.com>
2020-04-06 17:58:09 -0700

    add starter files in js, remove starter component in App.js

---
+++ client/js/App.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+class App extends Component {
+  render() {
+    return <Text>Thrival</Text>;
+  }
+}
+
+export default App;
