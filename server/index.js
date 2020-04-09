4f1ec27413b1351c0c50d6158b81cbe8f52f5893
Sina <sina202@gmail.com>
2020-04-08 21:49:32 -0700

    add resolvers, add jwttoken

--- server/index.js
+++ server/index.js
@@ -1,40 +1,40 @@
-// require("dotenv").config();
-// const jwt = require("jsonwebtoken");
-// const { ApolloServer } = require("apollo-server");
-// const typeDefs = require("./typeDefs");
-// const resolvers = require("./resolvers");
-// const { prisma } = require("./generated/prisma-client/index");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./typeDefs");
+const resolvers = require("./resolvers");
+const { prisma } = require("./generated/prisma-client/index");
 
-// const getUser = (token) => {
-//   try {
-//     if (token) {
-//       return jwt.verify(token, process.env.APP_SECRET);
-//     }
-//     return null;
-//   } catch (err) {
-//     return null;
-//   }
-// };
+const getUser = (token) => {
+  try {
+    if (token) {
+      return jwt.verify(token, process.env.APP_SECRET);
+    }
+    return null;
+  } catch (err) {
+    return null;
+  }
+};
 
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-//   context: ({ req }) => {
-//     const tokenWithBearer = req.headers.authorization || "";
-//     const token = tokenWithBearer.split(" ")[1];
-//     const user = getUser(token);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    const tokenWithBearer = req.headers.authorization || "";
+    const token = tokenWithBearer.split(" ")[1];
+    const user = getUser(token);
 
-//     return {
-//       user,
-//       prisma,
-//     };
-//   },
-// });
+    return {
+      user,
+      prisma,
+    };
+  },
+});
 
-// server
-//   .listen({
-//     port: 8383,
-//   })
-//   .then((info) =>
-//     console.log(`Server started on http://localhost:${info.port}`)
-//   );
+server
+  .listen({
+    port: 8383,
+  })
+  .then((info) =>
+    console.log(`Server started on http://localhost:${info.port}`)
+  );
