d3b266baf789fc6587b5dc2b464a3fb2206a35d4
Sina <sina202@gmail.com>
2020-04-08 20:53:06 -0700

    add auth screens to navigation

--- client/lib/navigation/index.js
+++ client/lib/navigation/index.js
@@ -7,19 +7,51 @@ import {DrawerNavStackScreens} from './DrawerNavigation';
 import Signup from '../screens/Signup';
 import Login from '../screens/Login';
 import CheckoutConfirmation from '../screens/CheckoutConfirmation';
+import {AuthContext} from '../context/AuthProvider';
+
+const AuthStack = createStackNavigator();
+
+const AuthStackScreens = (props) => {
+  return (
+    <AuthStack.Navigator initialRouteName="Login">
+      <AuthStack.Screen name="Login" component={Login} />
+      <AuthStack.Screen name="Signup" component={Signup} />
+    </AuthStack.Navigator>
+  );
+};
 
 const RootStack = createStackNavigator();
-const RootStackScreens = () => (
-  <RootStack.Navigator headerMode="none">
-    <RootStack.Screen name="Signup" component={Signup} />
-    {/* <RootStack.Screen name="Signup" component={Login} /> */}
-
-    <RootStack.Screen name="Main" component={BottomTabNavScreens} />
-    <RootStack.Screen name="DrawerNav" component={DrawerNavStackScreens} />
-    <RootStack.Screen name="Ticket" component={TicketScreen} />
-    <RootStack.Screen name="Notification" component={NotificationModal} />
-    <RootStack.Screen name="Confirmation" component={CheckoutConfirmation} />
-  </RootStack.Navigator>
-);
+const RootStackScreens = () => {
+  return (
+    <AuthContext.Consumer>
+      {({state}) => (
+        <RootStack.Navigator headerMode="none">
+          {!state.userToken ? (
+            <>
+              <RootStack.Screen name="Auth" component={AuthStackScreens} />
+            </>
+          ) : (
+            <>
+              <RootStack.Screen name="Main" component={BottomTabNavScreens} />
+              <RootStack.Screen
+                name="DrawerNav"
+                component={DrawerNavStackScreens}
+              />
+              <RootStack.Screen name="Ticket" component={TicketScreen} />
+              <RootStack.Screen
+                name="Notification"
+                component={NotificationModal}
+              />
+              <RootStack.Screen
+                name="Confirmation"
+                component={CheckoutConfirmation}
+              />
+            </>
+          )}
+        </RootStack.Navigator>
+      )}
+    </AuthContext.Consumer>
+  );
+};
 
 export default RootStackScreens;
