398f4d0502f75b76fa3e7a1f5d52192c5e479749
Ivan <ivandaixivwork@gmail.com>
2020-04-14 13:06:14 -0700

    Adds mutation for adding new job post

--- client/lib/screens/AddJobPost/AddJobPostContainer.js
+++ client/lib/screens/AddJobPost/AddJobPostContainer.js
@@ -1,29 +1,23 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import AddJobPost from './AddJobPost';
 import PropTypes from 'prop-types';
 import {Text} from 'react-native';
 import {Query} from '@apollo/react-components';
 import {gql} from 'apollo-boost';
 
-const ALL_USERS = gql`
-  query user($UserWhereUniqueInput: UserWhereUniqueInput!) {
-    user(where: $UserWhereUniqueInput) {
-      id
-      name
-      password
-      applicantProfile {
-        id
-        email
-        linkedin
-        github
-        appliedJobs {
-          id
-          createdAt
-          industry
-          location
-          discipline
-          totalRoles
-        }
+const UPDATE_JOBPOSTS = gql`
+  mutation updateEmployer(
+    $data: EmployerUpdateInput!
+    $where: EmployerWhereUniqueInput!
+  ) {
+    updateEmployer(data: $data, where: $where) {
+      jobpostings {
+        createdAt
+        industry
+        location
+        discipline
+        totalRoles
+        rate
       }
     }
   }
@@ -31,14 +25,43 @@ const ALL_USERS = gql`
 
 export default class AddJobPostContainer extends Component {
   render() {
+    const [industry, setIndustry] = useState();
+    const [location, setLocation] = useState();
+    const [discipline, setDiscipline] = useState();
+    const [totalRoles, setTotalRoles] = useState();
+    const [rate, setRate] = useState();
     return (
       <Query
-        query={ALL_USERS}
-        variables={{UserWhereUniqueInput: {id: 'ck8rr4sbd009h0810wcw4r12u'}}}>
+        query={UPDATE_JOBPOSTS}
+        variables={{
+          EmployerWhereUniqueInput: {id: 'ck8rr4sbd009h0810wcw4r12u'},
+          EmployerUpdateInput: {
+            data: {
+              jobpostings: {
+                createdAt,
+                industry,
+                location,
+                discipline: {
+                  set: discipline,
+                },
+                totalRoles,
+                rate,
+              },
+            },
+          },
+        }}>
         {({data, loading, error}) => {
           if (loading) return <Text>Loading...</Text>;
           if (error) return <Text>Error :(</Text>;
-          return <AddJobPost />;
+          return (
+            <AddJobPost
+              setIndustry={setIndustry}
+              setLocation={setLocation}
+              setDiscipline={setDiscipline}
+              setTotalRoles={setTotalRoles}
+              setRate={setRate}
+            />
+          );
         }}
       </Query>
     );
