a18e5f7b906e3d42b921caa50253ac1627b9d495
Ivan <ivandaixivwork@gmail.com>
2020-04-14 13:12:55 -0700

    Changes from hooks to stateful

--- client/lib/screens/AddJobPost/AddJobPostContainer.js
+++ client/lib/screens/AddJobPost/AddJobPostContainer.js
@@ -1,4 +1,4 @@
-import React, {Component, useState} from 'react';
+import React, {Component} from 'react';
 import AddJobPost from './AddJobPost';
 import PropTypes from 'prop-types';
 import {Text} from 'react-native';
@@ -24,17 +24,28 @@ const UPDATE_JOBPOSTS = gql`
 `;
 
 export default class AddJobPostContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      industry: 'Unavailable',
+      location: 'Unavailable',
+      discipline: 'Unavailable',
+      totalRoles: 'Unavailable',
+      rate: 'Unavailable',
+    };
+  }
   render() {
-    const [industry, setIndustry] = useState();
-    const [location, setLocation] = useState();
-    const [discipline, setDiscipline] = useState();
-    const [totalRoles, setTotalRoles] = useState();
-    const [rate, setRate] = useState();
+    // const [industry, setIndustry] = useState();
+    // const [location, setLocation] = useState();
+    // const [discipline, setDiscipline] = useState();
+    // const [totalRoles, setTotalRoles] = useState();
+    // const [rate, setRate] = useState();
     return (
       <Query
         query={UPDATE_JOBPOSTS}
         variables={{
-          EmployerWhereUniqueInput: {id: 'ck8rr4sbd009h0810wcw4r12u'},
+          // This id will be changed to user id when getting user data is setup
+          EmployerWhereUniqueInput: {id: 'ck8z1x9e8705s09816ly7r2ac'},
           EmployerUpdateInput: {
             data: {
               jobpostings: {
