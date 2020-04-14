1678f18a24b522d0f03226ab14cb3b0b46c17022
Ivan <ivandaixivwork@gmail.com>
2020-04-14 13:29:49 -0700

    Translate the component to stateless to use hooks

--- client/lib/screens/AddJobPost/AddJobPostContainer.js
+++ client/lib/screens/AddJobPost/AddJobPostContainer.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import AddJobPost from './AddJobPost';
 import PropTypes from 'prop-types';
 import {Text} from 'react-native';
-import {Query} from '@apollo/react-components';
+import {Mutation} from '@apollo/react-components';
 import {gql} from 'apollo-boost';
 
 const UPDATE_JOBPOSTS = gql`
@@ -23,61 +23,49 @@ const UPDATE_JOBPOSTS = gql`
   }
 `;
 
-export default class AddJobPostContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      industry: 'Unavailable',
-      location: 'Unavailable',
-      discipline: 'Unavailable',
-      totalRoles: 'Unavailable',
-      rate: 'Unavailable',
-    };
-  }
-  render() {
-    // const [industry, setIndustry] = useState();
-    // const [location, setLocation] = useState();
-    // const [discipline, setDiscipline] = useState();
-    // const [totalRoles, setTotalRoles] = useState();
-    // const [rate, setRate] = useState();
-    return (
-      <Query
-        query={UPDATE_JOBPOSTS}
-        variables={{
-          // This id will be changed to user id when getting user data is setup
-          EmployerWhereUniqueInput: {id: 'ck8z1x9e8705s09816ly7r2ac'},
-          EmployerUpdateInput: {
-            data: {
-              jobpostings: {
-                createdAt,
-                industry,
-                location,
-                discipline: {
-                  set: discipline,
-                },
-                totalRoles,
-                rate,
+const AddJobPostContainer = () => {
+  const [industry, setIndustry] = useState();
+  const [location, setLocation] = useState();
+  const [discipline, setDiscipline] = useState();
+  const [totalRoles, setTotalRoles] = useState();
+  const [rate, setRate] = useState();
+  return (
+    <Mutation
+      mutation={UPDATE_JOBPOSTS}
+      variables={{
+        // This id will be changed to user id when getting user data is setup
+        EmployerWhereUniqueInput: {id: 'ck8z1x9e8705s09816ly7r2ac'},
+        EmployerUpdateInput: {
+          data: {
+            jobpostings: {
+              industry,
+              location,
+              discipline: {
+                set: discipline,
               },
+              totalRoles,
+              rate,
             },
           },
-        }}>
-        {({data, loading, error}) => {
-          if (loading) return <Text>Loading...</Text>;
-          if (error) return <Text>Error :(</Text>;
-          return (
-            <AddJobPost
-              setIndustry={setIndustry}
-              setLocation={setLocation}
-              setDiscipline={setDiscipline}
-              setTotalRoles={setTotalRoles}
-              setRate={setRate}
-            />
-          );
-        }}
-      </Query>
-    );
-  }
-}
+        },
+      }}>
+      {(addJobPost, {data, error}) => {
+        if (error) return <Text>Error :(</Text>;
+        return (
+          <AddJobPost
+            addJobPost={addJobPost}
+            setIndustry={setIndustry}
+            setLocation={setLocation}
+            setDiscipline={setDiscipline}
+            setTotalRoles={setTotalRoles}
+            setRate={setRate}
+          />
+        );
+      }}
+    </Mutation>
+  );
+};
+export default AddJobPostContainer;
 
 AddJobPostContainer.propTypes = {
   navigation: PropTypes.object,
