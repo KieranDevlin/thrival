7122f970ccb7fa81721229123c5c7be8d04e53c8
Ivan <ivandaixivwork@gmail.com>
2020-04-15 15:54:09 -0700

    Adds styling the input based on theme

--- client/lib/screens/AddJobPost/styles.js
+++ client/lib/screens/AddJobPost/styles.js
@@ -1,28 +1,44 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignSelf: 'center',
   },
   textInput: {
-    backgroundColor: '#11185B',
     padding: 20,
     minHeight: 300,
   },
+  header: {
+    marginBottom: 10,
+    fontSize: 30,
+    fontWeight: '900',
+    color: '#11185B',
+    letterSpacing: 5,
+  },
+  title: {
+    alignSelf: 'center',
+    fontFamily: 'Raleway-Light',
+    color: '#2B2D42',
+    fontSize: 24,
+    fontWeight: '800',
+  },
   field: {
-    color: 'white',
-    backgroundColor: '#5877DD',
-    margin: 5,
-    padding: 10,
+    justifyContent: 'flex-start',
+    fontSize: 12,
+    borderWidth: 1,
+    borderColor: '#D2D9E5',
+    margin: 15,
+    color: '#2B2D42',
+    width: Dimensions.get('screen').width - 80,
+    padding: 8,
+    backgroundColor: '#EEF2F7',
   },
   picker: {
     margin: 5,
     position: 'relative',
-    backgroundColor: '#5877DD',
     padding: 20,
-    width: 'auto',
+    width: 200,
   },
 });
 
