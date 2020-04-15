36a4a5127de1a823e70e488c594353fd752c43bc
Sina <sina202@gmail.com>
2020-04-15 14:50:07 -0700

    add calendar screen and basic daypicker

--- client/lib/screens/Calendar/Calendar.js
+++ client/lib/screens/Calendar/Calendar.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {TouchableOpacity, ScrollView} from 'react-native-gesture-handler';
 import Text from '../../components/CustomText/CustomText';
+import CalendarPicker from 'react-native-calendar-picker';
 import {View} from 'react-native';
 import styles from './styles';
-import PropTypes from 'prop-types';
 
-const Calendar = ({navigation, route}) => {
-  return (
-    <View style={styles.container}>
-      <Text>Calendar</Text>
-    </View>
-  );
-};
+class Calendar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedStartDate: null,
+    };
+    this.onDateChange = this.onDateChange.bind(this);
+  }
 
-Calendar.propTypes = {
-  route: PropTypes.object,
-  navigation: PropTypes.objectOf(PropTypes.func),
-};
+  onDateChange(date) {
+    this.setState({
+      selectedStartDate: date,
+    });
+  }
+
+  render() {
+    const {selectedStartDate} = this.state;
+    const startDate = selectedStartDate
+      ? selectedStartDate.format('LLLL').toString()
+      : '';
+    return (
+      <View style={styles.container}>
+        <CalendarPicker
+          todayBackgroundColor="#f2e6ff"
+          selectedDayColor="#ED9421"
+          selectedDayTextColor="#FFFFFF"
+          onDateChange={this.onDateChange}
+        />
+
+        <View>
+          <Text>SELECTED DATE: {startDate}</Text>
+        </View>
+      </View>
+    );
+  }
+}
 
 export default Calendar;
