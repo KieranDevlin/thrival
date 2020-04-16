0141bb943e1881e76274431f3c48aa663660f5ea
Kieran Devlin <kjrdevlin@gmail.com>
2020-04-16 15:25:46 -0700

    Add mutations and pass data as props for myApplications

--- client/lib/screens/Job/Job.js
+++ client/lib/screens/Job/Job.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import moment from 'moment';
 import {
   TouchableOpacity,
@@ -7,12 +7,68 @@ import {
   Image,
   ScrollView,
   FlatList,
+  Animated,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import PropTypes from 'prop-types';
 import Text from '../../components/CustomText';
 import styles from './styles';
 import Button from '../../components/Button';
-const Job = ({job, faveIds, addFave, removeFave}) => {
+
+const Job = ({
+  job,
+  faveIds,
+  addFave,
+  removeFave,
+  updateApplications,
+  setJobId,
+  data,
+  error,
+}) => {
+  const [text, setText] = useState(null);
+  const yPositionAnimation = useRef(new Animated.Value(-50)).current;
+  const fadeAnimation = useRef(new Animated.Value(0)).current;
+  const moveIn = () => {
+    Animated.timing(yPositionAnimation, {
+      toValue: 0,
+      duration: 600,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const moveOut = () => {
+    Animated.timing(yPositionAnimation, {
+      toValue: -50,
+      duration: 600,
+      useNativeDriver: false,
+    }).start();
+  };
+  const fadeIn = () => {
+    Animated.timing(fadeAnimation, {
+      toValue: 1,
+      duration: 600,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const fadeOut = () => {
+    Animated.timing(fadeAnimation, {
+      toValue: 0,
+      duration: 600,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const animate = () => {
+    fadeIn();
+    moveIn();
+
+    setTimeout(() => {
+      fadeOut();
+      moveOut();
+    }, 2000);
+  };
+
   const renderDisciplines = (values) => {
     let str = '';
     for (let i = 0; i < values.length; i++) {
@@ -31,105 +87,140 @@ const Job = ({job, faveIds, addFave, removeFave}) => {
     }
     return link.slice(0, link.indexOf('/'));
   };
+
+  useEffect(() => {
+    if (error) {
+      setText(false);
+      animate();
+    }
+    if (data) {
+      setText(true);
+      animate();
+    }
+  }, [data, error]);
+
   return (
-    <ScrollView
-      showsVerticalScrollIndicator={false}
-      contentContainerStyle={styles.scroll}>
-      <View style={styles.jobPosting}>
-        <View style={styles.header}>
-          <Image
-            style={styles.logo}
-            source={{
-              uri: `https://logo.clearbit.com/${getDomain(
-                job.employer.contact.website,
-              )}`,
-            }}
-          />
+    <>
+      <Animated.View
+        style={[
+          styles.addedToFaves,
+          {top: yPositionAnimation, opacity: fadeAnimation},
+        ]}>
+        <Text style={styles.addText}>
+          {text ? 'Thanks for applying!' : 'There was an error applying!'}
+        </Text>
+      </Animated.View>
 
-          <View style={styles.infoContainer}>
-            <Text style={styles.h1}>{renderDisciplines(job.discipline)}</Text>
-            <Text style={styles.desc}>{job.location}</Text>
-            <Text style={styles.desc}>{job.companyName}</Text>
-            <Text style={styles.desc}>$20/hr</Text>
-          </View>
-        </View>
-
-        <View style={styles.main}>
-          <Text style={styles.h2}>Description</Text>
-          <Text style={styles.desc}>{job.description}</Text>
-
-          <Text style={styles.h2}>Roles and Responsibilities</Text>
-
-          <FlatList
-            data={job.roles}
-            renderItem={({item}) => (
-              <View style={styles.listItem}>
-                <Text>- {item}</Text>
-              </View>
-            )}
-            keyExtractor={(item) => item}
-          />
-
-          <Text style={styles.h2}>Recuirements</Text>
-
-          <FlatList
-            data={job.requirements}
-            renderItem={({item}) => (
-              <View style={styles.listItem}>
-                <Text>- {item}</Text>
-              </View>
-            )}
-            keyExtractor={(item) => item}
-          />
-
-          <View style={styles.cta}>
-            {faveIds.includes(job.id.toString()) ? (
-              <TouchableOpacity
-                onPress={() => {
-                  removeFave(job.id.toString());
-                }}>
-                <Icon
-                  style={styles.icon}
-                  name="star"
-                  size={50}
-                  color="#ED9421"
-                />
-              </TouchableOpacity>
-            ) : (
-              <TouchableOpacity
-                onPress={() => {
-                  addFave(job.id.toString());
-                }}>
-                <Icon
-                  style={styles.icon}
-                  name="star-outline"
-                  size={50}
-                  color="#D2D9E5"
-                />
-              </TouchableOpacity>
-            )}
-
-            <Button
-              onPress={() => {
-                Linking.openURL(job.employer.contact.website);
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.scroll}>
+        <View style={styles.jobPosting}>
+          <View style={styles.header}>
+            <Image
+              style={styles.logo}
+              source={{
+                uri: `https://logo.clearbit.com/${getDomain(
+                  job.employer.contact.website,
+                )}`,
               }}
-              style={styles.button}
-              bgcolor={'#11185B'}
-              label={'Apply'}
             />
+
+            <View style={styles.infoContainer}>
+              <Text style={styles.h1}>{renderDisciplines(job.discipline)}</Text>
+              <Text style={styles.desc}>{job.location}</Text>
+              <Text style={styles.desc}>{job.companyName}</Text>
+              <Text style={styles.desc}>$20/hr</Text>
+            </View>
           </View>
+
+          <View style={styles.main}>
+            <Text style={styles.h2}>Description</Text>
+            <Text style={styles.desc}>{job.description}</Text>
+
+            <Text style={styles.h2}>Roles and Responsibilities</Text>
+
+            <FlatList
+              data={job.roles}
+              renderItem={({item}) => (
+                <View style={styles.listItem}>
+                  <Text>- {item}</Text>
+                </View>
+              )}
+              keyExtractor={(item) => item}
+            />
+
+            <Text style={styles.h2}>Recuirements</Text>
+
+            <FlatList
+              data={job.requirements}
+              renderItem={({item}) => (
+                <View style={styles.listItem}>
+                  <Text>- {item}</Text>
+                </View>
+              )}
+              keyExtractor={(item) => item}
+            />
+
+            <View style={styles.cta}>
+              {faveIds.includes(job.id.toString()) ? (
+                <TouchableOpacity
+                  onPress={() => {
+                    removeFave(job.id.toString());
+                  }}>
+                  <Icon
+                    style={styles.icon}
+                    name="star"
+                    size={50}
+                    color="#ED9421"
+                  />
+                </TouchableOpacity>
+              ) : (
+                <TouchableOpacity
+                  onPress={() => {
+                    addFave(job.id.toString());
+                  }}>
+                  <Icon
+                    style={styles.icon}
+                    name="star-outline"
+                    size={50}
+                    color="#D2D9E5"
+                  />
+                </TouchableOpacity>
+              )}
+
+              <Button
+                onPress={() => {
+                  setJobId(job.id);
+                  updateApplications();
+                  Linking.openURL(job.employer.contact.website);
+                }}
+                style={styles.button}
+                bgcolor={'#11185B'}
+                label={'Apply'}
+              />
+            </View>
+          </View>
+          <Text>Posted {moment(job.createdAt).fromNow()}</Text>
+          <TouchableOpacity
+            style={styles.attribution}
+            onPress={() => {
+              Linking.openURL('https://clearbit.com');
+            }}>
+            <Text>Logos provided by Clearbit</Text>
+          </TouchableOpacity>
         </View>
-        <Text>Posted {moment(job.createdAt).fromNow()}</Text>
-        <TouchableOpacity
-          style={styles.attribution}
-          onPress={() => {
-            Linking.openURL('https://clearbit.com');
-          }}>
-          <Text>Logos provided by Clearbit</Text>
-        </TouchableOpacity>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </>
   );
 };
-
+Job.propTypes = {
+  job: PropTypes.object,
+  faveIds: PropTypes.array,
+  addFave: PropTypes.func,
+  removeFave: PropTypes.func,
+  updateApplications: PropTypes.func,
+  setJobId: PropTypes.func,
+  data: PropTypes.object,
+  error: PropTypes.object,
+};
 export default Job;
--- client/lib/screens/Job/JobContainer.js
+++ client/lib/screens/Job/JobContainer.js
@@ -1,51 +1,90 @@
-import React, {Component} from 'react';
+import React, {useState, useContext} from 'react';
 import {View} from 'react-native';
 import {Mutation} from '@apollo/react-components';
 import {gql} from 'apollo-boost';
+import PropTypes from 'prop-types';
 import Job from './Job';
 import styles from './styles';
 import {FavesContext} from '../../context/FavesContext';
-import Loader from '../../components/Loader';
-import Error from '../../components/Error';
+import {UserContext} from '../../context/UserContext';
+import {USER_WHERE_UNIQUE_ID} from '../UserProfile';
 
-const UPDATE_APPLICANT = gql`
-  mutation updateApplicant(
-    $data: ApplicantUpdateInput!
-    $where: ApplicantWhereUniqueInput!
-  ) {
-    updateApplicant(data: $data, where: $where) {
+const UPDATE_APPLICATIONS = gql`
+  mutation updateUser($data: UserUpdateInput!, $where: UserWhereUniqueInput!) {
+    updateUser(data: $data, where: $where) {
       id
-      appliedJobs {
+      applicantProfile {
         id
+        appliedJobs {
+          id
+          createdAt
+          rate
+          industry
+          location
+          discipline
+          employer {
+            id
+            linkedin
+            contact {
+              id
+              email
+              website
+            }
+          }
+        }
       }
     }
   }
 `;
 
-class JobContainer extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    console.log(this.props);
-    return (
-      // <Mutation mutation={UPDATE_APPLICANT} variables={{data,where:}}>
-
-      // </Mutation>
-      <FavesContext.Consumer>
-        {(value) => (
-          <View style={styles.container}>
-            <Job
-              job={this.props.route.params.job}
-              faveIds={value.faveIds}
-              addFave={value.addFaveJob}
-              removeFave={value.removeFaveJob}
-            />
-          </View>
-        )}
-      </FavesContext.Consumer>
-    );
-  }
-}
-
+const JobContainer = ({route}) => {
+  const {job} = route.params;
+  const {user} = useContext(UserContext);
+  const [jobId, setJobId] = useState('');
+  return (
+    <FavesContext.Consumer>
+      {(value) => (
+        <Mutation
+          mutation={UPDATE_APPLICATIONS}
+          refetchQueries={USER_WHERE_UNIQUE_ID}
+          variables={{
+            data: {
+              applicantProfile: {
+                update: {
+                  appliedJobs: {
+                    connect: {
+                      id: jobId,
+                    },
+                  },
+                },
+              },
+            },
+            where: {
+              id: user.id,
+            },
+          }}>
+          {(updateApplications, {error, data}) => {
+            return (
+              <View style={styles.container}>
+                <Job
+                  job={job}
+                  faveIds={value.faveIds}
+                  addFave={value.addFaveJob}
+                  removeFave={value.removeFaveJob}
+                  updateApplications={updateApplications}
+                  setJobId={setJobId}
+                  data={data}
+                  error={error}
+                />
+              </View>
+            );
+          }}
+        </Mutation>
+      )}
+    </FavesContext.Consumer>
+  );
+};
+JobContainer.propTypes = {
+  route: PropTypes.object,
+};
 export default JobContainer;
